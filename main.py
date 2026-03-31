377e9692660f7f05a8457b7bb0a3bb182171d19b
Hamza <faisalhamza909@gmail.com>
2026-03-31 10:07:48 +0200

    Initial start

---
+++ main.py
@@ -0,0 +1,6 @@
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+print(f"{board[0]} | {board[1]} | {board[2]}")
+print("_________")
+print(f"{board[3]} | {board[4]} | {board[5]}")
+print("_________")
+print(f"{board[6]} | {board[7]} | {board[8]}")
